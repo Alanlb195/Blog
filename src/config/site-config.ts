a35c7fb210733f6c7d89e0c84884b02cb300898a
alanlb195 <alanlopezbarrientos@live.com.mx>
2025-04-14 06:21:15 -0500

    Initial commit - content of the blog
    Initial commit - static content of the blog
    
    Initial commit - content of the blog
    q
    
    Initial commit - static content of the blog

---
+++ src/config/site-config.ts
@@ -0,0 +1,4 @@
+export const siteConfig = {
+    title: "Alan's Blog",
+    description: "Blog oficial de Alan Lopez - Fullstack web developer"
+}
---
+++ src/interfaces/frontmatter.ts
@@ -0,0 +1,9 @@
+export interface Frontmatter {
+    title: string;
+    date: Date;
+    description: string;
+    author: string;
+    image: string;
+    tags: string;
+    file: string;
+};
