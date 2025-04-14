2020faa449d47b108a766342f30640af1e18dac1
alanlb195 <alanlopezbarrientos@live.com.mx>
2025-04-14 10:41:39 -0500

    Feat - Authors Page

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,4 +1,4 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection, reference, z } from "astro:content";
 
 const blogCollection = defineCollection({
     type: 'content',
@@ -8,18 +8,29 @@ const blogCollection = defineCollection({
             date: z.date(),
             description: z.string(),
             image: image(),
-            // image: image().refine((img) => img.width < 1200, {
-            //     message: 'Image should be lower than 1200px',
-            // }),
 
             // Relacion
-            author: z.string(),
+            // author: z.string(),
+            author: reference('author'),
 
             // Relacion
             tags: z.array(z.string()),
+            
+            isDraft: z.boolean().default(false),
         })
 });
 
+
+const authorCollection = defineCollection({
+    type: 'data',
+    schema: ({ image }) =>
+        z.object({
+            name: z.string(),
+            avatar: image()
+        })
+})
+
 export const collections = {
     blog: blogCollection,
+    author: authorCollection,
 }
