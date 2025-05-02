6bb6433862d5bea5ff5e8f73c6fa3cf483334c63
alanlb195 <alanlopezbarrientos@live.com.mx>
2025-05-02 17:44:42 -0500

    Feat - RSS Feed configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,9 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   vite: {
-      plugins: [tailwindcss()],
+    plugins: [tailwindcss()],
   },
 
   integrations: [mdx()],
+  site: 'https://example.com',
 });
